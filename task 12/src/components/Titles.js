3608290c36f3ca1ba66d0333d98ee3960ba82b04
Raphael-mb <53257515+Raphael-mb@users.noreply.github.com>
2019-10-10 15:29:35 +0200

    Add files via upload

---
+++ task 12/src/components/Titles.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+const Titles = () => (
+	<div>
+		<h1 className="weatherHeading">Weather App</h1>
+		<p className="weatherHeading">Enter values to get weather for a particular city</p>
+	</div>
+);
+
+export default Titles;
